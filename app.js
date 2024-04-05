aca32083f56f505209a1d7a1f5a6d48efeed0e97
suk-6 <me@suk.kr>
2024-04-05 14:18:21 +0900

    Fix day increment logic in getmeal route

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ app.get('/getmeal', async (req, res) => {
 
     const day = new Date();
     for (i = 0; i < 3; i++) {
-        if (day.getDay() === 0 || day.getDay() === 6) day.setDate(day.getDate() + 1);
+        if (day.getDay() === 6) day.setDate(day.getDate() + 1);
+        if (day.getDay() === 0) day.setDate(day.getDate() + 1);
         await neisService.getDiet(day).then((result) => {
             data[i] = result
         });
