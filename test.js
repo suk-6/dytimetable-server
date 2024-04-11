ff8335f73bad67fb551049f89429f8dd394a5571
suk-6 <me@suk.kr>
2024-04-11 15:29:22 +0900

    Update push notification click action and data

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 const push = require('./push');
 
-push.sendNotificationByTopic(`1-10`, `테스트 title`, `테스트 body`, { click_action: "test" });
+push.sendNotificationByTopic(`1-10`, `테스트 title`, `테스트 body`, { click_action: "notice1", data: "0" });
