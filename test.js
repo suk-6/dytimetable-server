ba1a14edc364edab868f9ccb1e500bad41628cc2
suk-6 <me@suk.kr>
2024-04-26 12:21:32 +0900

    Test: Push notification replacement

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 const push = require('./push');
 
-push.sendNotificationByTopic(`1-10`, `테스트 title`, `테스트 body`, { click_action: "url", data: "https://suk.kr" });
+// push.sendNotificationByTopic(`1-10`, `테스트 title`, `테스트 body`, { click_action: "url", data: "https://suk.kr", type: "period" });
+push.sendNotificationByTopic('period',`1-10`, `테스트 title4 - period`, `테스트 body`);
